46c8597d4bfcfd1584757361b4850ad3f7d1f51a
doublejkim <jaejin.km@gmail.com>
2023-02-22 16:04:23 +0900

    second commit

--- discordbot.py
+++ discordbot.py
@@ -6,3 +6,25 @@ app = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 @app.event
 async def on_ready():
     print('f{app.user.name} 연결 성공')
+    await app.change_presence(status=discord.Status.online, activity=None)
+
+@app.command()
+async def hello(ctx):
+    await ctx.send('Hello..World..')
+
+@app.command(aliases=['따라하기', 'f'])
+async def follow(ctx, *args):
+    for i in args:
+        await ctx.send(f'```hello {i} world..```')
+
+#@app.event
+#async def on_message(msg):
+#    await msg.add_reaction("^^")  # imogi...
+
+# token..
+# .discordbot_token 파일에 토큰 입력 필요. 
+f = open(".discordbot_token", 'r')
+token = f.readline()
+f.close()
+
+app.run(token)
